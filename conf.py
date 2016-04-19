5647e8fb93ac719f81b1822938eedc0bb9661719
meker12 <meker@hotmail.com>
2016-04-18 20:56:27 -0500

    Update exclude patterns to resolve build errors
    Fix duplicate topic errors caused by included files.

--- conf.py
+++ conf.py
@@ -69,7 +69,7 @@ release = '1'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build', 'samples']
+exclude_patterns = ['_build', '*/getting-started/samples/*', '_common/*']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
@@ -100,7 +100,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'classic'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
